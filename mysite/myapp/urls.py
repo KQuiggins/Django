893110d31807be25bde91b391a6dc9b8de4c937a
Kenneth Quiggins <kashfamilyvalues@gmail.com>
2022-11-04 15:26:01 -0400

    created image upload form

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -6,5 +6,6 @@ app_name='myapp'
 urlpatterns = [
     path('', views.index),
     path('products/', views.products),
-    path('products/<int:id>', views.product_detail, name='product_detail')
+    path('products/<int:id>', views.product_detail, name='product_detail'),
+    path('products/add/', views.add_product, name='add_product'),
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Product
 
+
 def index(request):
     return HttpResponse("Hello World")
+
 
 def products(request):
     products = Product.objects.all()
@@ -12,9 +14,22 @@ def products(request):
     }
     return render(request, 'myapp/index.html', context)
 
+
 def product_detail(request, id):
     product = Product.objects.get(id=id)
-    context={
-        'product':product
+    context = {
+        'product': product
     }
     return render(request, 'myapp/detail.html', context)
+
+
+def add_product(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        price = request.POST.get('price')
+        desc = request.POST.get('desc')
+        image = request.FILES['upload']
+        product = Product(name=name, price=price, desc=desc, image=image)
+        product.save()
+
+    return render(request, 'myapp/addproduct.html')
